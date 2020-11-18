be6b70ad24b68bf8ed29c12b8bde67ea28c05122
Ubuntu <ubuntu@ip-172-31-48-242.ec2.internal>
2020-11-18 00:07:21 +0000

    added helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Ryan Chung Kam Chung All rights reserved
+//
+// Created by Ryan Chung Kam Chung
+// Created in November 2020
+// "Hello, World!", proper style, in CPP
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+}
